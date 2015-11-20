eaf828a6cab2550a1df81cdef8dd6120a7a61720
Fedor Buyakov <cloud@4-asp.net>
2015-11-21 00:05:52 +0300

    Test async/await feature of TypeScript.

--- src/.bin/program.ts
+++ src/.bin/program.ts
@@ -3,8 +3,14 @@ import "source-map-support/register";
 import * as yahoo from "../yahoo/client";
 import * as lib from "../lib";
 
-yahoo.syncFinancials()
-	.then(() => lib.log("done"))
-	.catch(err => lib.log(err.stack));
+async function main() {
+	try {
+		await yahoo.syncFinancials();
+		lib.log("done");
+	}
+	catch (err) {
+		lib.log(err.stack);
+	}
+}
 
-//lib.log("!!!");
+main();
